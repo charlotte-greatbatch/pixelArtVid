a315ec868703c77c1545bb0919c3a5d58bed543f
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-07-02 19:10:25 +1200

    added circle picker

--- pixels/src/components/Editor.js
+++ pixels/src/components/Editor.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../styles/editor.scss'
+import { CirclePicker } from 'react-color'
 
 export default function Editor() {
   const [panelWidth, setPanelWidth] = useState(16)
@@ -9,34 +10,62 @@ export default function Editor() {
   const [buttonText, setButtonText] = useState('start drawing')
   const [selectedColor, setColor] = useState('#f44336')
 
+  function initialise() {
+    setHideOptions(!hideOptions)
+    setHideDrawingPanel(!hideDrawingPanel)
+
+    buttonText === 'start drawing'
+      ? setButtonText('reset')
+      : setButtonText('start drawing')
+  }
+
+  function changeColor(color) {
+    setColor(color.hex)
+  }
+
   return (
     <div id="editor">
       <h1>Pixel Editor</h1>
-      <h2>Enter Panel Dimensions</h2>
-      <div id="options">
-        <div className="option">
-          <input
-            type="number"
-            className="panelInput"
-            defaultValue="16"
-            // onChange={}
-          />
-          <span>Width</span>
-        </div>
-      </div>
-      <div id="options">
-        <div className="option">
-          <input
-            type="number"
-            className="panelInput"
-            defaultValue="16"
-            // onChange={}
-          />
-          <span>Height</span>
-        </div>
-      </div>
+      {/* if hide drawing panel is true, want to show this title, but when clicked start drawing it'll hide  */}
+      {hideDrawingPanel && <h2>Enter Panel Dimensions</h2>}
+      {hideDrawingPanel && (
+        <>
+          <div id="options">
+            <div className="option">
+              <input
+                type="number"
+                className="panelInput"
+                defaultValue={panelWidth}
+                onChange={(e) => {
+                  setPanelWidth(e.target.value)
+                }}
+              />
+              <span>Width</span>
+            </div>
+          </div>
+          <div id="options">
+            <div className="option">
+              <input
+                type="number"
+                className="panelInput"
+                defaultValue={panelHeight}
+                onChange={(e) => {
+                  setPanelHeight(e.target.value)
+                }}
+              />
+              <span>Height</span>
+            </div>
+          </div>
+        </>
+      )}
 
-      <button className="button">Start Drawing</button>
+      <button onClick={initialise} className="button">
+        {buttonText}
+      </button>
+
+      {hideOptions && (
+        <CirclePicker color={selectedColor} onChangeComplete={changeColor} />
+      )}
     </div>
   )
 }
